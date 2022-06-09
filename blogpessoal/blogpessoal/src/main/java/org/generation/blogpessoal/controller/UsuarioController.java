36cd4b41174d65f28b9dcce97d0a81508981f0c1
Gabriele Sá <93798476+psgabriele@users.noreply.github.com>
2022-06-09 11:30:50 -0300

    Adicionando métodos Get e Put em UsuarioController

--- blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
+++ blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
@@ -1,15 +1,22 @@
 package org.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.generation.blogpessoal.model.UserLogin;
 import org.generation.blogpessoal.model.Usuario;
+import org.generation.blogpessoal.repository.UsuarioRepository;
 import org.generation.blogpessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +29,21 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Authentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
@@ -32,5 +54,10 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
 	}
 }
